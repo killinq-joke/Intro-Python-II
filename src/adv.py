2d2fbd06d22ae942c5708bfe9eee0b36232dfc29
zakaria touzri <zakadmaster@gmail.com>
2020-07-07 16:27:14 +0200

    ADDED: base location

--- src/adv.py
+++ src/adv.py
@@ -40,8 +40,7 @@ print(room["outside"].n_to)
 
 # Make a new player object that is currently in the 'outside' room.
 name = input("What's your name? ")
-player1 = Player(name, "location")
-print(player1)
+player1 = Player(name, "outside")
 # Write a loop that:
 #
 # * Prints the current room name
