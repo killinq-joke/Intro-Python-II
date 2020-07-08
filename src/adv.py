fad1a87ae0808706d7e0fea351e644de61de09a1
zakaria touzri <zakadmaster@gmail.com>
2020-07-09 01:12:22 +0200

    ADDED: mvp

--- src/adv.py
+++ src/adv.py
@@ -7,17 +7,17 @@ room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer':    Room("foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow':   Room("narrow", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room("treasure", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -59,7 +59,7 @@ print(room["foyer"])
 def start_game():
 
     while True:
-        location = player1.location.lower()
+        location = player1.location
         print(f"You are currently here: {location}")
         print(f"{room[location].description}")
 
