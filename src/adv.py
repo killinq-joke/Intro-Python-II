d7fc0a8ca2e1a501334dbd8b270bf4f4acc8926c
zakaria touzri <zakadmaster@gmail.com>
2020-07-09 16:16:01 +0200

    ADDED: items to rooms

--- src/adv.py
+++ src/adv.py
@@ -7,21 +7,21 @@ import textwrap
 
 room = {
     'outside':  Room("outside", "Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", ["flashlight"]),
 
     'foyer':    Room("foyer", "Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", ["cutter"]),
 
     'overlook': Room("overlook", "Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", ["battery"]),
 
     'narrow':   Room("narrow", "Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", ["rock"]),
 
     'treasure': Room("treasure", "Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", ["empty treasure"]),
 }
 
 # Link rooms together
